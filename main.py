b0e56bb325db884d7cf527d04a3e37a1a991ae74
prashant9430 <prashantkumarpandey9430@gmail.com>
2026-01-19 22:13:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,3 +47,8 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
